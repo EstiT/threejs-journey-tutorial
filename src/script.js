9657bc05ea65d45555b464c687614c9b7f31aafc
Esti <estictweg@hotmail.com>
2022-01-11 22:24:51 +1030

    7 - cameras, click and drag control

--- src/script.js
+++ src/script.js
@@ -1,52 +1,17 @@
 import './style.css'
 import * as THREE from 'three'
 import gsap from 'gsap'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-// Scene
 const scene = new THREE.Scene()
-
-// const axesHelper = new THREE.AxesHelper(2)
-// scene.add(axesHelper)
 
 // Object
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 mesh.position.set(0.7, 0, 0.5)
-// mesh.scale.x = 2
-// mesh.scale.y = 0.25
-// mesh.scale.z = 0.5
-// mesh.rotation.x = Math.PI * 0.25
-// mesh.rotation.y = Math.PI * 0.25
 scene.add(mesh)
 
-/*
-const group = new THREE.Group()
-group.scale.y = 2
-group.rotation.y = 0.2
-scene.add(group)
-
-const cube1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
-cube1.position.x = - 1.5
-group.add(cube1)
-
-const cube2 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
-cube2.position.x = 0
-group.add(cube2)
-
-const cube3 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
-cube3.position.x = 1.5
-group.add(cube3)
-*/
 
 // Sizes
 const sizes = {
@@ -57,34 +22,46 @@ const sizes = {
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
-// camera.lookAt(mesh.position)
+camera.lookAt(mesh.position)
 scene.add(camera)
 
+
+
 // Renderer
+const canvas = document.querySelector('canvas.webgl');
 const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('canvas.webgl')
+    canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
 
+const controls = new OrbitControls(camera, canvas) // allows click and drag shape
+// controls.target.y = 2
+controls.enableDamping = true
 
+const cursor = {
+    x: 0,
+    y: 0
+}
 const clock = new THREE.Clock()
-gsap.to(mesh.position, { duration: 1, delay: 1, x: -0.5 })
 const tick = () =>
 {
-    const elapsedTime = clock.getElapsedTime()
+    // const elapsedTime = clock.getElapsedTime()
 
-    // Update objects
-    mesh.rotation.y = elapsedTime
-    // mesh.position.x = Math.cos(elapsedTime)
-    // mesh.position.y = Math.sin(elapsedTime)
-
-    // camera.position.x = Math.cos(elapsedTime)
-    // camera.position.y = Math.sin(elapsedTime)
-    // camera.lookAt(mesh.position)
-
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2
+    // camera.position.y = cursor.y * 3
+    camera.lookAt(mesh.position)
+    controls.update()
     renderer.render(scene, camera)
     window.requestAnimationFrame(tick)
 }
 
 tick()
+
+
+window.addEventListener('mousemove', (event) =>
+{
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = -(event.clientY / sizes.height - 0.5)
+})
