f94f41ad94c113eff7856ad85a8a41a63bbab3a0
Esti <estictweg@hotmail.com>
2022-01-13 10:59:06 +1030

    13 - 3D text and random floating donuts

--- src/script.js
+++ src/script.js
@@ -3,7 +3,8 @@ import * as THREE from 'three'
 import gsap from 'gsap'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
-
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
 
 const loadingManager = new THREE.LoadingManager()
 loadingManager.onStart = () =>
@@ -23,35 +24,6 @@ loadingManager.onError = () =>
     console.log('loading error')
 }
 
-const textureLoader = new THREE.TextureLoader(loadingManager)
-const colorTexture = textureLoader.load('/textures/minecraft.png')
-// colorTexture.repeat.x = 2
-// colorTexture.repeat.y = 3
-// colorTexture.wrapS = THREE.RepeatWrapping //x axis
-// colorTexture.wrapT = THREE.RepeatWrapping //y
-// colorTexture.rotation =  Math.PI * 0.25
-colorTexture.generateMipmaps = false
-colorTexture.minFilter = THREE.NearestFilter
-colorTexture.magFilter = THREE.NearestFilter
-const alphaTexture = textureLoader.load('/textures/door/alpha.jpg')
-const heightTexture = textureLoader.load('/textures/door/height.jpg')
-const normalTexture = textureLoader.load('/textures/door/normal.jpg')
-const ambientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
-const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
-const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
-const gradientTexture = textureLoader.load('/textures/gradients/3.jpg')
-const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
-
-const matcapTexture = textureLoader.load('/textures/matcaps/1.png')
-// const matcapTexture = textureLoader.load('/textures/matcaps/3.png')
-// const matcapTexture = textureLoader.load('/textures/matcaps/4.png')
-// const matcapTexture = textureLoader.load('/textures/matcaps/5.png')
-// const matcapTexture = textureLoader.load('/textures/matcaps/6.png')
-// const matcapTexture = textureLoader.load('/textures/matcaps/7.png')
-// const matcapTexture = textureLoader.load('/textures/matcaps/8.png')
-
-const material = new THREE.MeshMatcapMaterial()
-material.matcap = matcapTexture
 
 
 const parameters = {
@@ -64,38 +36,73 @@ const parameters = {
 
 const scene = new THREE.Scene()
 
-
-const geometry = new THREE.BoxGeometry(1, 1, 1)
 const materialLam = new THREE.MeshLambertMaterial()
-const materialToon = new THREE.MeshToonMaterial()
-materialToon.gradientMap = gradientTexture
 
-// const matcapTexture = new THREE.MeshMatcapMaterial()
-// const material = new THREE.MeshNormalMaterial()
-// material.transparent = true
-// material.opacity = 0.5
-material.flatShading = true
-material.alphaMap = doorAlphaTexture
-// material.side = THREE.DoubleSide
-material.matcap = matcapTexture
+
 const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(0.5, 16, 16),
-    material
+    materialLam
 )
 sphere.position.x = - 1.5
 
 const plane = new THREE.Mesh(
-    new THREE.PlaneGeometry(1, 1),
+    new THREE.BoxGeometry(1, 1),
     materialLam
 )
 
 const torus = new THREE.Mesh(
     new THREE.TorusGeometry(0.3, 0.2, 16, 32),
-    materialToon
+    materialLam
 )
 torus.position.x = 1.5
 
-scene.add(sphere, plane, torus)
+scene.add(sphere)
+
+const textureLoader = new THREE.TextureLoader(loadingManager)
+const fontLoader = new FontLoader()
+
+fontLoader.load(
+    '/fonts/helvetiker_regular.typeface.json',
+    (font) =>
+    {
+        const textGeometry = new TextGeometry(
+            'Hello Three.js',
+            {
+                font: font,
+                size: 0.5,
+                height: 0.2,
+                curveSegments: 12,
+                bevelEnabled: true,
+                bevelThickness: 0.03,
+                bevelSize: 0.02,
+                bevelOffset: 0,
+                bevelSegments: 5
+            }
+        )
+        const matcapTexture = textureLoader.load('/textures/matcaps/1.png')
+        const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
+        textGeometry.computeBoundingBox()
+        textGeometry.center()
+        const text = new THREE.Mesh(textGeometry, material)
+        scene.add(text)
+        
+        const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
+        for(let i = 0; i < 100; i++)
+        {
+            
+            const donut = new THREE.Mesh(donutGeometry, material)
+            donut.position.x = (Math.random() - 0.5) * 10
+            donut.position.y = (Math.random() - 0.5) * 10
+            donut.position.z = (Math.random() - 0.5) * 10
+            donut.rotation.x = Math.random() * Math.PI
+            donut.rotation.y = Math.random() * Math.PI
+            const scale = Math.random()
+            donut.scale.set(scale, scale, scale)
+            scene.add(donut)
+        }
+    }
+)
+
 
 
 // Sizes
@@ -121,7 +128,6 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
 
 const controls = new OrbitControls(camera, canvas) // allows click and drag shape
-// controls.target.y = 2
 controls.enableDamping = true
 
 const cursor = {
@@ -133,10 +139,6 @@ const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
-    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2
-    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2
-    // camera.position.y = cursor.y * 3
-    // camera.lookAt(mesh.position)
     sphere.rotation.y = 0.1 * elapsedTime
     plane.rotation.y = 0.1 * elapsedTime
     torus.rotation.y = 0.1 * elapsedTime
@@ -208,9 +210,6 @@ window.addEventListener('dblclick', () =>
 
 // Debug
 const gui = new dat.GUI()
-// gui.add(mesh.position, 'y').min(- 3).max(3).step(0.01).name('elevation')
-// gui.add(mesh, 'visible')
-// gui.add(material, 'wireframe')
 gui.addColor(parameters, 'color')    
     .onChange(() =>
     {
